a23e9f9fec74b642f9b02bef02d6a370b4acdfa6
Sigee <sigee15@gmail.com>
2025-07-22 08:30:59 +0200

    Format code

--- core/src/main/java/org/apache/stormcrawler/persistence/urlbuffer/AbstractURLBuffer.java
+++ core/src/main/java/org/apache/stormcrawler/persistence/urlbuffer/AbstractURLBuffer.java
@@ -81,8 +81,7 @@ public abstract class AbstractURLBuffer implements URLBuffer {
 
         // create the queue if it does not exist
         // and add the url
-        queues.computeIfAbsent(key, k -> new LinkedList<>())
-                .add(new URLMetadata(URL, m));
+        queues.computeIfAbsent(key, k -> new LinkedList<>()).add(new URLMetadata(URL, m));
         return in_buffer.add(URL);
     }
 
